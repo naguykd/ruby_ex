c2d2dc85786ccafb41addfebb8d1aecc5103a7a3
naguykd <naguydaoudi@gmail.com>
2018-09-27 11:33:03 +0200

    exercices ruby 6 à 8

---
+++ exo_07c.rb
@@ -0,0 +1,6 @@
+user_name = gets.chomp
+puts user_name
+
+# plus ou moins pas de différence, car la fonction gets.chomp permet d'interagir avec l'user
+# dans exo_07b, le fait de poser une question et de mettre ">" permet de mieux comprendre l'interaction plutot qu'en cliquant sur return
+
---
+++ exo_08.rb
@@ -0,0 +1,6 @@
+#demande le prénom de l'utilisateur, et qui salue l'utilisateur avec "Bonjour, prénom !"
+
+puts "Bonjour, c'est quoi ton blase ?"
+print "> "
+user_name = gets.chomp
+puts "Bonjour, #{user_name} !"
